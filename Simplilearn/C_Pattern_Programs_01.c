d6c7b66fc35f14d46cad76bb2b89863bd3c2ec2b
Timothy Boettinger <tboettinger@my.waketech.edu>
2025-07-02 21:54:19 -0400

    Update C_Pattern_Programs_01.c
    Pattern 1: Right-Angle Triangle

--- Simplilearn/C_Pattern_Programs_01.c
+++ Simplilearn/C_Pattern_Programs_01.c
@@ -3,16 +3,16 @@
 int main(){
   int rows = 5;
   
-   for (int i = 1; i <= rows; ++i) { //Outer loop for rows  
+  for (int i = 1; i <= rows; ++i) { //Outer loop for rows  
   
-    for (int j = 1; j <= i; ++j){  //Inner loop for Columns
+  for (int j = 1; j <= i; ++j){  //Inner loop for Columns
   
-     printf("* "); //Print * for Stars
+  printf("* "); //Print * for Stars
  
-            }
+  }
 
   printf("\n"); //New line
 
- }
+  }
 
 }
