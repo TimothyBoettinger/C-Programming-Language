80385360bea20d5975cab4363bdc94cc2249baed
Timothy Boettinger <tboettinger@my.waketech.edu>
2025-07-02 22:14:46 -0400

    Update C_Pattern_Programs_21.c

--- Simplilearn/C_Pattern_Programs_21.c
+++ Simplilearn/C_Pattern_Programs_21.c
@@ -1,1 +1,45 @@
+//Pattern 21: Half Diamond Star Pattern
+#include<stdio.h>
+
+int main()  
+
+{  
+
+    int rows=5,i,j;  
+
+    //prints upper half diamond
+
+    for( i=1;i<=rows;i++)  
+
+    {  
+
+      for( j=1;j<=i;j++)
+
+      {
+
+          printf("*");
+
+      }
+
+      printf("\n"); 
+
+     } 
+
+    //prints lower-half diamond
+
+    for( i=rows-1;i>=1;i--){
+
+       for( j=1;j<=i;j++){
+
+           printf("*");  
+
+            }  
+
+       printf("\n");  
+
+    }     
+
+    return 0;  
+
+}
 
