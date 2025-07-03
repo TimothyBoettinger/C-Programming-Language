76ae16bd607dcfc40029fcee7bc362c87a47bf25
Timothy Boettinger <tboettinger@my.waketech.edu>
2025-07-02 22:15:49 -0400

    Create C_Pattern_Programs_19.c

---
+++ Simplilearn/C_Pattern_Programs_19.c
@@ -0,0 +1,26 @@
+//Pattern 19: Square Star Pattern
+#include <stdio.h>  
+
+ void main()  
+
+{  
+
+    int rows=5;      
+
+    for(int i=0;i<rows;i++)  
+
+    {  
+
+        for(int j=0;j<rows;j++)  
+
+        {  
+
+            printf("*");  
+
+        }  
+
+        printf("\n");  
+
+    }        
+
+}  
