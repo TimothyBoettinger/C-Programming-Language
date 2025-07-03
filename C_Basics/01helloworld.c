7152b790c8bdf5a4336c66ff49ef328f8322b0b0
Timothy Boettinger <tboettinger@my.waketech.edu>
2025-07-02 22:56:15 -0400

    Update 01helloworld.c

--- C_Basics/01helloworld.c
+++ C_Basics/01helloworld.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-int main(void)
-{
-    printf("Hello World");
-    return 0;  
+//Structure of C Program, think paragraph in a paper
+#include <stdio.h>//Header, what do i need to recall(in this case an I/o Library)
+int main(void)//Main Body
+{//Just a seperator telling the body to run below
+    printf("Hello World");//Statement, PRINT THIS!, then the CPU says print what? So you include Hello World
+    return 0;  //Gotta tell the CPU to stop, otherwise it wont, its like a baby, thus treat with care
 }
